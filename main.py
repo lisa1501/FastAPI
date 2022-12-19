adc6b91a04f8d52d72dd5c67be08c03c1ef849f9
Lisa S <halisasaipulla@gmail.com>
2022-12-18 17:18:49 -0800

    First HTTP Post Requests

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import imp
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-def get_posts():
-    return {"data": "This is my first post."}
-@app.get("/")
 def root():
     return {"message": "Hello World!"}
 
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is my first post."}
+
+@app.post("/createposts")
+def create_posts():
+    return {"message": "succefully created posts"}
