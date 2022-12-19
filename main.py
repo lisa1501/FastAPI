9d4d2af2bd684281b2071c53a5c5939c161029df
Lisa S <halisasaipulla@gmail.com>
2022-12-18 16:36:15 -0800

    Path operations: first post

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Hello World!"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is my first post."}
