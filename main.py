dced080dd6691d8b2b2de5f0ea62af416149d306
Lisa S <halisasaipulla@gmail.com>
2022-12-19 10:07:11 -0800

    Implement update  operation

--- main.py
+++ main.py
@@ -52,9 +52,6 @@ def get_post(id: int):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    #deleting post
-    #find the index in the array that has required ID
-    #my_posts.pop(index)
     index = find_index_post(id)
     
     if index == None:
@@ -62,3 +59,19 @@ def delete_post(id: int):
                             detail=f"post with id: {id} does not exists")
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id:int, post:Post):
+    index = find_index_post(id)
+    
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exists")
+    
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
+
+    return {"message":"updated post"}
