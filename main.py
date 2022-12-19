4daa750f4fe94968e8b36d976c7ef821c776e55a
Lisa S <halisasaipulla@gmail.com>
2022-12-18 19:24:10 -0800

    Schema Validation with Pydentic

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating:Optional[int] = None
 
 @app.get("/")
 def root():
@@ -13,6 +21,8 @@ def get_posts():
     return {"data": "This is my first post."}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title {payload['title']} content:{payload['content']}"}
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data": post}
+# title str, content str
