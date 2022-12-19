1a7c58444331bb6da4169f31f0c0a2ad3640479e
Lisa S <halisasaipulla@gmail.com>
2022-12-18 16:52:16 -0800

    The first path operation match is always going to be the one runs

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
+def get_posts():
+    return {"data": "This is my first post."}
+@app.get("/")
 def root():
     return {"message": "Hello World!"}
 
-@app.get("/posts")
-def get_posts():
-    return {"data": "This is my first post."}
