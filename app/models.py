de7a3985c045d7eb4763fe02c355fc5b7f6c055a
Lisa S <halisasaipulla@gmail.com>
2022-12-19 16:37:27 -0800

    Adding CreatedAt Column

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from email.policy import default
+from sqlite3 import Timestamp
 from xmlrpc.client import Boolean
 from sqlalchemy import Column,Integer,String,Boolean
-from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
 class Post(Base):
@@ -10,6 +12,7 @@ class Post(Base):
     id = Column(Integer,primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
 
