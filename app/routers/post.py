6faa25892dfe43814a8f9dd1bcfef014864dd9eb
Lisa S <halisasaipulla@gmail.com>
2022-12-22 11:31:23 -0800

    assigning owner id when creating new post

--- app/routers/post.py
+++ app/routers/post.py
@@ -20,8 +20,8 @@ def get_posts(db:Session = Depends(get_db),user_id: int =Depends(oauth2.get_curr
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db:Session = Depends(get_db),current_user: int =Depends(oauth2.get_current_user) ):
-    print(current_user.email)
-    new_post = models.Post(**post.dict())
+  
+    new_post = models.Post(owner_id = current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
--- app/schemas.py
+++ app/schemas.py
@@ -15,8 +15,8 @@ class PostCreate(PostBase):
 
 class Post(PostBase):
     id: int
-    
     created_at: datetime
+    owner_id: int
 
     class Config:
         orm_mode = True
