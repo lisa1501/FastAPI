8bc98c4b8168d57547f8ff2bf565164bf1a48631
Lisa S <halisasaipulla@gmail.com>
2023-01-03 11:37:52 -0800

    Get one post with Joins

--- app/routers/post.py
+++ app/routers/post.py
@@ -34,16 +34,18 @@ def create_posts(post: schemas.PostCreate, db:Session = Depends(get_db),current_
     return new_post
 
 
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, db:Session = Depends(get_db),current_user: int =Depends(oauth2.get_current_user)):
     
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    print(post)
+    # post = db.query(models.Post).filter(models.Post.id == id).first()
+    # print(post)
+    post =db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote,models.Vote.post_id == models.Post.id,
+    isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post wiht id: {id} was not found")
-    if post.owner_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+    # if post.owner_id != current_user.id:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
                             
     return post
 
