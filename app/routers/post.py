ee5df1b8815fa5b5eee6edc7ea8a1c43ecfd6d6a
Lisa S <halisasaipulla@gmail.com>
2023-01-03 11:31:47 -0800

    joins in SqlAlchemy

--- app/routers/post.py
+++ app/routers/post.py
@@ -4,6 +4,7 @@ from fastapi import  Response,status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session  
 from .. database import get_db
 from typing import  List, Optional
+from sqlalchemy import func
 
 
 router =APIRouter(
@@ -11,10 +12,15 @@ router =APIRouter(
     tags=['Posts'] 
 )
 
-@router.get("/",response_model=List[schemas.Post]) 
+@router.get("/",response_model=List[schemas.PostOut]) 
+# @router.get("/")
 def get_posts(db:Session = Depends(get_db),current_user: int =Depends(oauth2.get_current_user), limit: int = 10, skip: int =0, search: Optional[str]=""):
 
-    posts= db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    # posts= db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote,models.Vote.post_id == models.Post.id,
+    isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+
     return posts
 
 
