fc27a753361aa7525d46f750f9c0d08e380edd54
Lisa S <halisasaipulla@gmail.com>
2023-01-06 10:29:42 -0800

    parametrize

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,6 +1,13 @@
+import pytest
 from app.calculations import add 
 
-def test_add():
+@pytest.mark.parametrize("num1, num2, expected", [
+    (3,2,5), 
+    (5,3,8),
+    (-10,-2,-12)
+
+])
+def test_add(num1, num2, expected):
     print("testing add function")
-    assert add(5,3) == 8
+    assert add(num1,num2) == expected
 
