63507e9cbddb43be21f158308947fbe8df6cca11
Lisa S <halisasaipulla@gmail.com>
2023-01-08 11:24:12 -0800

    added test_user fixture

--- tests/database.py
+++ tests/database.py
@@ -16,8 +16,9 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def session():
+    print("my session fixture ran")
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
     db = TestingSessionLocal()
@@ -26,7 +27,7 @@ def session():
     finally:
         db.close()
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def client(session):
     def override_get_db():
     
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,25 @@
+import pytest
 from app import schemas
 from .database import client,session
 
-def test_root(client):
-    res = client.get("/")
-    print(res.json().get('message'))
-    assert res.json().get('message') == 'Hello world!'
-    assert res.status_code == 200
+@pytest.fixture
+def test_user(client):
+    user_data={"email": "lisa@gmail.com", 
+                        "password": "password1234"}
+    res = client.post("/users",json=user_data)
+
+    assert res.status_code == 201
+    new_user= res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+# def test_root(client):
+#     res = client.get("/")
+#     print(res.json().get('message'))
+#     assert res.json().get('message') == 'Hello world!'
+#     assert res.status_code == 200
+
+
 
 def test_create_user(client):
     res = client.post(
@@ -15,8 +29,10 @@ def test_create_user(client):
     assert new_user.email == "hello@gmail.com"
     assert res.status_code == 201
 
-def test_login_user(client):
+
+def test_login_user(test_user,client):
     res = client.post(
-        "/login", data={"username":"hello@gmail.com","password": "password1234"}
+        "/login", data={"username":test_user['email'],"password": test_user['password']}
     )
     assert res.status_code  == 200
+
