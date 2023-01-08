e80a8138c809acb1c5ad25c9c8d1f029ce8867ff
Lisa S <halisasaipulla@gmail.com>
2023-01-08 11:31:41 -0800

    successfully completed user login test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,8 @@
 import pytest
+from jose import jwt
 from app import schemas
 from .database import client,session
+from app.config import settings
 
 @pytest.fixture
 def test_user(client):
@@ -32,7 +34,11 @@ def test_create_user(client):
 
 def test_login_user(test_user,client):
     res = client.post(
-        "/login", data={"username":test_user['email'],"password": test_user['password']}
-    )
+        "/login", data={"username":test_user['email'],"password": test_user['password']})
+    login_res=schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
     assert res.status_code  == 200
 
